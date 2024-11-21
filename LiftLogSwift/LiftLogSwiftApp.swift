f3fd7389b8693b03beca885e23b5b10d18cfb276
n-dorazio <natedorazio@gmail.com>
2024-11-21 15:14:54 -0700

    Initial Commit

---
+++ LiftLogSwift/LiftLogSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLogSwiftApp.swift
+//  LiftLogSwift
+//
+//  Created by Nathaniel D'Orazio on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct LiftLogSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiftLogSwiftTests/LiftLogSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLogSwiftTests.swift
+//  LiftLogSwiftTests
+//
+//  Created by Nathaniel D'Orazio on 2024-11-21.
+//
+
+import Testing
+@testable import LiftLogSwift
+
+struct LiftLogSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
