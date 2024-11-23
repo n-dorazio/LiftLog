5acf343f184899008de177626e751e74162c709d
LC <luke.chen@ucalgary.ca>
2024-11-22 18:26:02 -0700

    made profile field edit popup screens

---
+++ LiftLogSwift/UI/EditGenderView.swift
@@ -0,0 +1,27 @@
+//
+//  EditGenderView.swift
+//  LiftLogSwift
+//
+//  Created by Luke CHEN on 2024-11-22.
+//
+
+import SwiftUI
+
+struct EditGenderView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var userProfile: UserProfileModel
+    var body: some View {
+        HStack {
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "chevron.left")
+                    .font(.title2)
+            }
+            Spacer()
+        }
+        .padding(.horizontal)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
